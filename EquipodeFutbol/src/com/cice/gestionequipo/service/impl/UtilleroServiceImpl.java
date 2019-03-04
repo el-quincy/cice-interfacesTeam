1845ece5050fa49c7610da91e9ed60950fe66e24
pedrodlf <pedrodlafuente@gmail.com>
2019-03-04 21:53:28 +0100

    Revert "Update UtilleroServiceImpl.java"
    This reverts commit a79057fcea2c270246403c5c3c7b5a393e38c1dc.

--- EquipodeFutbol/src/com/cice/gestionequipo/service/impl/UtilleroServiceImpl.java
+++ EquipodeFutbol/src/com/cice/gestionequipo/service/impl/UtilleroServiceImpl.java
@@ -17,7 +17,16 @@ public class UtilleroServiceImpl implements UtilleroService {
 			System.out.println("El Nombre de "+personal.getNombre()+" Coincide");
 			System.out.println("El Apellido de "+personal.getApellido()+" Coincide");
 		personal.setFechaIncorporacion(fechaInicio);
-		dataSource.plantilla.put(id, personaltgiorfgmk
+		dataSource.plantilla.put(id, personal);
+		System.out.println(personal.toString()+ " Contratado");
+		}else{
+		System.out.println(personal.toString()+" no contratada");
+		}
+		System.out.println("Verificando fecha de Entrada "
+		+ dataSource.plantilla.get(id).getFechaIncorporacion());
+	}
+
+	@Override
 	public void despedir(Integer fechaFin, int id) {
 		// TODO Auto-generated method stub
 		
