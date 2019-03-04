40ce0bea06fdd6b1912992cb146a8746900c348c
PacLega <delpi280990@hotmail.com>
2019-03-04 23:59:58 +0100

    Update Main.java

--- EquipodeFutbol/src/com/cice/gestionequipo/Main.java
+++ EquipodeFutbol/src/com/cice/gestionequipo/Main.java
@@ -1,11 +1,6 @@
 package com.cice.gestionequipo;
 
 
-
-
-import com.cice.gestionequipo.service.UtilleroService;
-import com.cice.gestionequipo.service.impl.UtilleroServiceImpl;
-
 public class Main {
 // estoy modificando
 	public static void main(String[] args) {
