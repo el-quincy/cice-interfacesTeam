f0276e7a218439f475fdc4056f8459d32147722c
pedrodlf <pedrodlafuente@gmail.com>
2019-03-04 20:59:15 +0100

    Update EntrenadorService.java

--- EquipodeFutbol/src/com/cice/gestionequipo/service/EntrenadorService.java
+++ EquipodeFutbol/src/com/cice/gestionequipo/service/EntrenadorService.java
@@ -2,7 +2,7 @@ package com.cice.gestionequipo.service;
 
 import java.util.List;
 
-public interface EntrenadorService {
+public interface EntrenadorService extends PersonalService{
 
 	public List<String> recuperarPalmares(int Id);
 	public void agregarTitulo(String titulo);
