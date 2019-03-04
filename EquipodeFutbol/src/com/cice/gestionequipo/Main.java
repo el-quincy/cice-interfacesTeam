ab3fac4c6d7d1352894eb79791e3543089e9a2a2
bsantamaria <borjasantamaria19@gmail.com>
2019-03-04 21:04:16 +0100

    Update Main.java

--- EquipodeFutbol/src/com/cice/gestionequipo/Main.java
+++ EquipodeFutbol/src/com/cice/gestionequipo/Main.java
@@ -1,13 +1,7 @@
 package com.cice.gestionequipo;
 
 public class Main {
-<<<<<<< HEAD
-	
-	//modifico
 
-=======
-// estoy modificando
->>>>>>> dev-pre
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
