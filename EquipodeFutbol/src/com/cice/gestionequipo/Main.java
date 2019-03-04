5d2e459b8b5b7949239f0a6eb068d1490b23ba66
bsantamaria <borjasantamaria19@gmail.com>
2019-03-04 21:54:36 +0100

    Revert "toca"
    This reverts commit cb096b48810e9475d56e94e55edd59c08354c5c2, reversing
    changes made to 3caaf6b3c7a537d03c31715c0f32dd5bccfb9a7c.

--- EquipodeFutbol/src/com/cice/gestionequipo/Main.java
+++ EquipodeFutbol/src/com/cice/gestionequipo/Main.java
@@ -1,12 +1,9 @@
 package com.cice.gestionequipo;
 
-import com.cice.gestionequipo.service.UtilleroService;
-import com.cice.gestionequipo.service.impl.UtilleroServiceImpl;
-
 public class Main {
 
 	public static void main(String[] args) {
-		
+		// TODO Auto-generated method stub
 
 	}
 
