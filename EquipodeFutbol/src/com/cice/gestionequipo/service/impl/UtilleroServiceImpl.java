fb6848b6fd6cf585ca91f9f60a27feef0fd6736c
pedrodlf <pedrodlafuente@gmail.com>
2019-03-04 21:42:36 +0100

    contratar implementado
    Ya podeis disfrutar

--- EquipodeFutbol/src/com/cice/gestionequipo/service/impl/UtilleroServiceImpl.java
+++ EquipodeFutbol/src/com/cice/gestionequipo/service/impl/UtilleroServiceImpl.java
@@ -1,5 +1,47 @@
 package com.cice.gestionequipo.service.impl;
 
-public class UtilleroServiceImpl {
+import com.cice.gestionequipo.dao.DataSource;
+import com.cice.gestionequipo.entidades.Personal;
+import com.cice.gestionequipo.service.UtilleroService;
+
+public class UtilleroServiceImpl implements UtilleroService {
+
+	DataSource dataSource = new DataSource(); 
+	@SuppressWarnings("static-access")
+	@Override
+	public void contratar(Integer fechaInicio, int id, String name, String surname) {
+		
+		Personal personal = dataSource.plantilla.get(id);
+		if(name.equalsIgnoreCase(personal.getNombre())
+				&&surname.equalsIgnoreCase(personal.getApellido())){
+			System.out.println("El Nombre de "+personal.getNombre()+" Coincide");
+			System.out.println("El Apellido de "+personal.getApellido()+" Coincide");
+		personal.setFechaIncorporacion(fechaInicio);
+		dataSource.plantilla.put(id, personal);
+		System.out.println(personal.toString()+ " Contratado");
+		}else{
+		System.out.println(personal.toString()+" no contratada");
+		}
+		System.out.println("Verificando fecha de Entrada "
+		+ dataSource.plantilla.get(id).getFechaIncorporacion());
+	}
+
+	@Override
+	public void despedir(Integer fechaFin, int id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void modificarSalario(double nuevoSalario) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void modificacionEstadoCapacidadDeViajar(int idUtillero, boolean nuevoEstado) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
