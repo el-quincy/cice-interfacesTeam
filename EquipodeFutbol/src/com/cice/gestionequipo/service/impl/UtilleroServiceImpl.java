445e7a22c9f7ebde3b2d6ae5b6e5c29234255cd6
CRISTINA RENDON RIVERA <CRISPI@MacBook-Pro-de-CRISTINA-2.local>
2019-03-05 19:15:22 +0100

    com

---
+++ EquipodeFutbol/src/com/cice/gestionequipo/service/impl/UtilleroServiceImpl.java
@@ -0,0 +1,34 @@
+package com.cice.gestionequipo.service.impl;
+import com.cice.gestionequipo.service.UtilleroService;
+
+
+
+public class UtilleroServiceImpl implements UtilleroService{
+
+	
+	
+	@Override
+	public void contratar(Integer fechaInicio, int id, String name, String surname) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void despedir(Integer fechaFin, int id) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void modificarSalario(double nuevoSalario) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void modificacionEstadoCapacidadDeViajar(int idUtillero, boolean nuevoEstado) {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
