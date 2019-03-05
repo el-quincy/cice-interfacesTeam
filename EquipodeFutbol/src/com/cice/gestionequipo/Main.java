4798bd08c3073c7697925839cf7660f141f2cec7
Gabriel6v <gabrielvicente6a@gmail.com>
2019-03-05 20:37:02 +0100

    Comit

--- EquipodeFutbol/src/com/cice/gestionequipo/Main.java
+++ EquipodeFutbol/src/com/cice/gestionequipo/Main.java
@@ -8,10 +8,7 @@ public class Main {
 // estoy modificando
 	public static void main(String[] args) {
 		
-//	PersonalServiceImpl prueba = new PersonalServiceImpl();
-//	prueba.contratar(1551812941, 1, "Pep", "Guardiola");
-	System.out.println("==============");
-	prueba.despedir(1551812957, 36);
+
 
 	}
 
