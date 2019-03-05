7b996081869e92f40fd0dd56c7efb882c0fe2d13
suanamateo <47611850+suanamateo@users.noreply.github.com>
2019-03-05 19:21:19 +0100

    Refactorizacion de JugadorServiceImpl y EntrenadorServiceImpl
    Se han añadido 2 implementaciones que heredan: EntrenadorServiceImpl de PersonalServiceImpl y JugadorServiceImpl de EntrenadorServiceImpl.-

---
+++ EquipodeFutbol/src/com/cice/gestionequipo/service/impl/EntrenadorServiceImpl.java
@@ -0,0 +1,28 @@
+package com.cice.gestionequipo.service.impl;
+
+import java.util.List;
+
+import com.cice.gestionequipo.service.EntrenadorService;
+
+public class EntrenadorServiceImpl extends PersonalServiceImpl implements EntrenadorService{
+
+	@Override
+	public List<String> recuperarPalmares(int Id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void agregarTitulo(String titulo) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void agregarListadeEquiposAnteriores(int id, List<String> equipos) {
+		// TODO Auto-generated method stub
+		
+	}
+	
+
+}
---
+++ EquipodeFutbol/src/com/cice/gestionequipo/service/impl/JugadorServiceImpl.java
@@ -0,0 +1,30 @@
+package com.cice.gestionequipo.service.impl;
+
+import java.util.List;
+
+import com.cice.gestionequipo.entidades.Paritido;
+import com.cice.gestionequipo.service.JugadorService;
+
+
+public class JugadorServiceImpl extends EntrenadorServiceImpl implements JugadorService {
+
+	@Override
+	public void seleccionarTitularProximoPartido() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public List<Paritido> getPartidosComoConvocado() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Paritido> getPartidosComoTitular() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	
+}
