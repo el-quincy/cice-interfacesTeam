9cce44ac735c6e41411cd5592c2d979c5ef3afee
Gabriel6v <gabrielvicente6a@gmail.com>
2019-03-05 20:20:13 +0100

    Metodo Despedir Implementado

--- EquipodeFutbol/src/com/cice/gestionequipo/Main.java
+++ EquipodeFutbol/src/com/cice/gestionequipo/Main.java
@@ -1,13 +1,17 @@
 package com.cice.gestionequipo;
 
 import com.cice.gestionequipo.service.UtilleroService;
+import com.cice.gestionequipo.service.impl.PersonalServiceImpl;
 import com.cice.gestionequipo.service.impl.UtilleroServiceImpl;
 
 public class Main {
 // estoy modificando
 	public static void main(String[] args) {
 		
-	
+	PersonalServiceImpl prueba = new PersonalServiceImpl();
+	prueba.contratar(1551812941, 1, "Pep", "Guardiola");
+	System.out.println("==============");
+	prueba.despedir(1551812957, 36);
 
 	}
 
--- EquipodeFutbol/src/com/cice/gestionequipo/service/impl/PersonalServiceImpl.java
+++ EquipodeFutbol/src/com/cice/gestionequipo/service/impl/PersonalServiceImpl.java
@@ -1,41 +1,56 @@
 package com.cice.gestionequipo.service.impl;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import com.cice.gestionequipo.dao.DataSource;
 import com.cice.gestionequipo.entidades.Personal;
 import com.cice.gestionequipo.service.PersonalService;
 
-public class PersonalServiceImpl implements PersonalService{
-	DataSource dataSource = new DataSource(); 
-	@SuppressWarnings("static-access")
+public class PersonalServiceImpl implements PersonalService {
+	DataSource dataSource = new DataSource();
+
 	@Override
 	public void contratar(Integer fechaInicio, int id, String name, String surname) {
-		
+
 		Personal personal = dataSource.plantilla.get(id);
-		if(name.equalsIgnoreCase(personal.getNombre())
-				&&surname.equalsIgnoreCase(personal.getApellido())){
-			System.out.println("El Nombre de "+personal.getNombre()+" Coincide");
-			System.out.println("El Apellido de "+personal.getApellido()+" Coincide");
-		personal.setFechaIncorporacion(fechaInicio);
-		dataSource.plantilla.put(id, personal);
-		System.out.println(personal.toString()+ " Contratado");
-		}else{
-		System.out.println(personal.toString()+" no contratada");
+		if (name.equalsIgnoreCase(personal.getNombre()) && surname.equalsIgnoreCase(personal.getApellido())) {
+			System.out.println("El Nombre de " + personal.getNombre() + " Coincide");
+			System.out.println("El Apellido de " + personal.getApellido() + " Coincide");
+			personal.setFechaIncorporacion(fechaInicio);
+			dataSource.plantilla.put(id, personal);
+			System.out.println(personal.toString() + " Contratado");
+		} else {
+			System.out.println(personal.toString() + " no contratada");
 		}
-		System.out.println("Verificando fecha de Entrada "
-		+ dataSource.plantilla.get(id).getFechaIncorporacion());
-		
+		System.out.println("Verificando fecha de Entrada " + dataSource.plantilla.get(id).getFechaIncorporacion());
+
 	}
 
 	@Override
 	public void despedir(Integer fechaFin, int id) {
-		// TODO Auto-generated method stub
+		Personal personal = dataSource.plantilla.get(id);
+		if(id>0&&id<18){
+			if (personal.getFechaIncorporacion() != null&&personal.getId() == id) {
+				System.out.println("Existe el empleado.");
+				personal.setFechaDespido(fechaFin);
+				System.out.println("El empleado con la id: " + id + " ha sido despedido a fecha de: " + fechaFin);
+				
+			} else {
+				System.out.println("No existe el empleado");
+			}
+		} else {
+			System.out.println("No puedes introducir este id");
+		}
 		
+
 	}
 
 	@Override
 	public void modificarSalario(double nuevoSalario) {
 		// TODO Auto-generated method stub
-		
+
 	}
 
 }
